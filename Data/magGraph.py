1661434e63c061a3574d13950480be0099cf792d
Sam <samroq@yahoo.com>
2017-05-20 15:40:56 -0500

    Fixed magGraph

--- Data/magGraph.py
+++ Data/magGraph.py
@@ -15,9 +15,9 @@ accz = range(dataPts)
 for i in range(len(words)/37):			#Loop amount of data point times
 	time[i] = float(words[i*37])/1000	#Set first var = the time converted to sec
 						#Use this to add acc in all axies acc[i] = float(words[(i*37)+20]) + float(words[(i*37)+22]) + float(words[(i*37)+24])	#Set the second var to whatever you want, in this case all of the acc added up
-	accx[i] = float(words[(i*37)+2])	#Add 20, 22, 24 for non calculated values
-	accy[i] = float(words[(i*37)+4])	#Add 14, 16, 18 for the calculated values
-	accz[i] = float(words[(i*37)+6])
+	accx[i] = float(words[(i*37)+26])	#Add 20, 22, 24 for non calculated values
+	accy[i] = float(words[(i*37)+28])	#Add 14, 16, 18 for the calculated values
+	accz[i] = float(words[(i*37)+30])
 
 linex, = plt.plot(time, accx, label="X")	#Plot time vs acc on the graph for x y and z axies
 liney, = plt.plot(time, accy, label="Y")
